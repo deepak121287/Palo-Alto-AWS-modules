3e41feebda8e2aa7430c2b4bdfe563121410d684
deepak121287 <deepak121287@hotmail.com>
2024-05-17 23:12:38 +0530

    security group added

--- security groups/variables.tf
+++ security groups/variables.tf
@@ -1,1 +1,21 @@
+variable "name" {}
+variable "vpc_id" {}
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
 
+variable "egress_rules" {
+  description = "List of egress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
