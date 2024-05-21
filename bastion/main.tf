0317baeaefae8ee16b86b1b84a73b707fff53981
deepak121287 <deepak121287@hotmail.com>
2024-05-21 23:56:30 +0530

    added bastion module

--- bastion/main.tf
+++ bastion/main.tf
@@ -8,16 +8,23 @@ resource "aws_instance" "bastion" {
               yum update -y
               yum install -y git
               cd /home/ec2-user
-              git clone https://github.com/<your-username>/<your-repo>.git
-              chmod +x /home/ec2-user/<your-repo>/<your-script>.sh
-              /home/ec2-user/<your-repo>/<your-script>.sh
+              git clone https://github.com/deepak121287/Palo-Alto-PANOS-modules.git
+              chmod +x /home/ec2-user/Palo-Alto-PANOS-modules/configuration.py
+              chmod +x /home/ec2-user/Palo-Alto-PANOS-modules/PA-keypair.pem
               EOF
-
+  network_interface {
+  device_index          = 0
+  network_interface_id  = aws_network_interface.bastion_nic.id
+  }
   tags = {
     Name = "BastionHost"
   }
 }
 
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
+resource "aws_network_interface" "bastion_nic" {
+  subnet_id       = var.data_subnet_id
+  security_groups = var.security_group_ids
+  tags = {
+    Name = "${var.instance_name}-data"
+  }
 }
