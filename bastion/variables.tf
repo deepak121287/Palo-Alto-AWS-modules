0d5c02efabbfecdf3c0bed9965910004a4379720
Deepak Negi <deepak.negi@happiestminds.com>
2024-06-12 16:50:33 +0530

    test

--- bastion/variables.tf
+++ bastion/variables.tf
@@ -5,5 +5,4 @@ variable bastion_mgmt_subnet_id {}
 variable bastion_data_subnet_id {}
 variable bastion_security_group_ids {}
 variable bastion_instance_name {}
-variable region {}
 variable iam_role_name {}
